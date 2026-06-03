9d25bd76a80251120d17604672be74b9d984107d
Osdam <oscar_mancipemo@fet.edu.co>
2026-06-03 11:11:32 -0500

    movi el front

--- frontend/src/app/models/user-role.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum UserRole {
-  SUPER_ADMIN = 'Super Admin',
-  ADMINISTRADOR = 'Administrador',
-  ADMINISTRATIVO = 'Administrativo',
-  RECEPCION = 'Recepción',
-  ENTRENADOR = 'Entrenador',
-  MIEMBRO = 'Miembro',
-}
